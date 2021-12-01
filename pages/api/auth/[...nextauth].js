7946078fe19615f05202c4bc54593cb87d861d55
Marlon Clay <luciousmc@icloud.com>
2021-12-01 14:16:14 -0800

    adds jwt logic for spotify

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
 import { LOGIN_URL } from '../../../lib/spotify';
+import { refreshAccessToken } from '../../../services/spotify';
 
 export default NextAuth({
   providers: [
@@ -13,5 +14,34 @@ export default NextAuth({
   secret: process.env.JWT_SECRET,
   pages: {
     signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          userName: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        };
+      }
+
+      // If token has not expired, return the token
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
+      // If token has expired, refresh
+      return await refreshAccessToken(token);
+    },
+
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
+    },
   },
 });
---
+++ services/spotify.js
@@ -0,0 +1,26 @@
+import spotifyApi from '../lib/spotify';
+
+export const refreshAccessToken = async (token) => {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+
+    console.log(`refreshedToken`, refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ...token,
+      error: 'Refresh Token Error',
+    };
+  }
+};
