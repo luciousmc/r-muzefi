79802e373d893dcc14313864088b4274f1df3fcf
Marlon Clay <luciousmc@icloud.com>
2021-12-01 22:51:26 -0800

    creates hook to randomize gradient color

--- components/MainContent/MainContent.js
+++ components/MainContent/MainContent.js
@@ -1,16 +1,30 @@
+import { ChevronDownIcon } from '@heroicons/react/outline';
 import { useSession } from 'next-auth/react';
+import useRandomColor from '../../hooks/useRandomColor';
 
 function MainContent() {
   const { data: session } = useSession();
+  const { randomColor } = useRandomColor();
 
   return (
-    <div className='flex flex-grow text-white'>
-      <h1>Main Contenttttt</h1>
-      <header>
-        <div>
-          <img src={session?.user.image} alt='User Image' />
+    <div className='flex-grow text-white'>
+      <header className='absolute top-5 right-8'>
+        <div className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+          <img
+            className='rounded-full w-10 h-10'
+            src={session?.user.image}
+            alt='User Image'
+          />
+          <h2>{session?.user.name}</h2>
+          <ChevronDownIcon className='h-5 w-5' />
         </div>
       </header>
+
+      <section
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${randomColor} h-80 text-white p-8`}
+      >
+        {/* <img src="" alt="" /> */}
+      </section>
     </div>
   );
 }
---
+++ hooks/useRandomColor.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+export default function useRandomColor() {
+  const [randomColor, setRandomColor] = useState(null);
+
+  const colors = [
+    'from-pink-500',
+    'from-purple-500',
+    'from-green-500',
+    'from-blue-500',
+    'from-yellow-500',
+    'from-indigo-500',
+    'from-pink-500',
+  ];
+
+  useEffect(() => {
+    setRandomColor(colors[Math.floor(Math.random() * colors.length)]);
+  }, []);
+
+  return { randomColor };
+}
